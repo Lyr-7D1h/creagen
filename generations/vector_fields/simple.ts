f8e31a202ac6bba10a5ec5a7267babf14c2b00d3
Lyr <lyr-7d1h@pm.me>
2024-10-28 00:56:20 +0100

    box muller rng

--- generations/vector_fields/simple.ts
+++ generations/vector_fields/simple.ts
@@ -1,29 +1,29 @@
-import { Vector, vec, svg } from 'genart'
+import { Vector, vec, Svg } from 'genart'
 
 function field({ x, y }: Vector<2>) {
-  return vec(y, -x + y)
+  return vec(y, -(x ** 2) + y)
 }
 
 const WIDTH = 1000
 const HEIGHT = 1000
-const s = svg({ width: WIDTH, height: HEIGHT })
+const s = Svg.create({ width: WIDTH, height: HEIGHT })
 
 let points = []
-for (let x = -5; x <= 5; x++) {
-  for (let y = -5; y <= 5; y++) {
+for (let x = -10; x <= 10; x += 0.5) {
+  for (let y = -10; y <= 10; y += 0.5) {
     points.push(vec(x, y))
   }
 }
 
 function coord({ x, y }: Vector<2>) {
-  return vec(x * 50 + WIDTH / 2, y * 50 + HEIGHT / 2)
+  return vec(x * 40 + WIDTH / 2, y * 40 + HEIGHT / 2)
 }
 
 for (const p of points) {
   const { x, y } = coord(p)
-  s.circle(2, x, y)
+  // s.circle(2, x, y)
 }
-s.circle(2, WIDTH / 2, HEIGHT / 2, { fill: 'red', stroke: 'red' })
+// s.circle(2, WIDTH / 2, HEIGHT / 2, { fill: 'red', stroke: 'red' })
 
 for (const p of points) {
   const v = field(p).norm()
