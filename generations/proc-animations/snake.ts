3e141f6280224e84168de1f8c870cd4d93e7c44e
Lyr <lyr-7d1h@pm.me>
2024-12-17 00:17:35 +0100

    snake

--- generations/proc-animations/snake.ts
+++ generations/proc-animations/snake.ts
@@ -1,9 +1,9 @@
-import { Canvas, vec, Color, rng } from 'genart'
+import { Math, Canvas, vec, Color, rng, Vector } from 'genart'
 
 const canvas = Canvas.create()
 const rand = rng()
 
-let body = []
+let body: Vector<2>[] = []
 for (let i = 0; i < 20; i++) {
   body.push(vec(50, 50))
 }
@@ -15,11 +15,6 @@ let maxVelocity = 200 / 1000
 let angularVelocity = (1 / 4) * Math.PI
 let acceleration = 0.0001
 
-const bounds = [
-  [0, canvas.width],
-  [0, canvas.height],
-]
-
 draw((dt) => {
   canvas.clear()
 
@@ -30,6 +25,10 @@ draw((dt) => {
   if (rand.random() > 0.95) {
     acceleration = rand.integer(-1, 1) * 0.0001
   }
+  // if (isNaN(velocity.x)) {
+  //   console.log(velocity, angularVelocity)
+  //   return
+  // }
 
   velocity[0] += angularVelocity * dt
   velocity[1] += acceleration * dt
@@ -40,26 +39,31 @@ draw((dt) => {
   // calculate how much to move into direction
   const [angle, v] = velocity
   const d = vec(dt * v * Math.cos(angle), dt * v * Math.sin(angle))
+  console.log(body)
 
-  body[0].add(d).wraparound(bounds as any)
+  body[0].add(d)
   for (let i = 1; i < body.length; i++) {
     let prev = body[i - 1]
     let cur = body[i]
     const dist = Math.sqrt(prev.dist2(cur))
+    // console.log(dist)
     const diff = dist - jointDistance
-    if (diff > 0) {
-      const d = prev.clone().sub(cur).norm().mul(diff)
-      cur.add(d).wraparound(bounds as any)
-    }
+    const d = prev.clone().sub(cur).norm().mul(diff)
+    cur.add(d)
   }
 
   // draw
   for (const p of body) {
-    canvas.circle(p, 20, {
-      fill: Color.BLACK,
-      stroke: Color.BLACK,
-      strokeWidth: 2,
-    })
+    canvas.circle(
+      Math.mod(p.x, canvas.width),
+      Math.mod(p.y, canvas.height),
+      20,
+      {
+        fill: Color.BLACK,
+        stroke: Color.BLACK,
+        strokeWidth: 2,
+      }
+    )
   }
 
   canvas.load()
